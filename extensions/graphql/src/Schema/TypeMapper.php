653df8b4abec945584c908c8709a830f6416bfed
Lenny Obez <github@lennyobez.com>
2026-02-23 13:58:46 +0100

    feat(ext): implement GraphQL extension with schema-first design

---
+++ extensions/graphql/src/Schema/TypeMapper.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pulsar\Extension\Graphql\Schema;
+
+use Pulsar\Api\Api;
+use Pulsar\Extension\Cms\Content\ContentType;
+
+/**
+ * Maps CMS field types and content types to GraphQL type names.
+ */
+#[Api(since: '1.0.0')]
+final readonly class TypeMapper
+{
+    /**
+     * Map a CMS field type string to a GraphQL scalar type.
+     */
+    public static function mapFieldType(string $cmsFieldType): string
+    {
+        return match ($cmsFieldType) {
+            'int', 'integer' => 'Int',
+            'float', 'double', 'decimal' => 'Float',
+            'bool', 'boolean' => 'Boolean',
+            'datetime', 'date', 'json', 'text', 'string' => 'String',
+            default => 'String',
+        };
+    }
+
+    /**
+     * Map a CMS content type enum to a GraphQL-safe type name suffix.
+     */
+    public static function mapContentType(ContentType $contentType): string
+    {
+        return match ($contentType) {
+            ContentType::Article => 'Article',
+            ContentType::Page => 'Page',
+        };
+    }
+}
