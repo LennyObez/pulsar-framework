3e57a363a7f94cc07c984ac66c935c401a939478
Lenny Obez <github@lennyobez.com>
2026-05-19 09:48:38 +0200

    refactor(feature-flag): shape-typed @param in FlagDefinition fromArray

--- src/FeatureFlag/FlagDefinition.php
+++ src/FeatureFlag/FlagDefinition.php
@@ -33,38 +33,28 @@ final readonly class FlagDefinition
     /**
      * Create a FlagDefinition from a raw array.
      *
-     * @param array<string, mixed> $data
+     * @param array{
+     *     enabled?: bool|int|string,
+     *     type?: string,
+     *     percentage?: int,
+     *     allowed_tenants?: list<string>,
+     *     allowed_users?: list<string>,
+     *     allowed_environments?: list<string>,
+     *     description?: string,
+     * } $data
      */
     #[NoDiscard]
     public static function fromArray(string $name, array $data): self
     {
-        /** @var string $typeValue */
-        $typeValue = $data['type'] ?? 'boolean';
-
-        /** @var list<string> $allowedTenants */
-        $allowedTenants = $data['allowed_tenants'] ?? [];
-
-        /** @var list<string> $allowedUsers */
-        $allowedUsers = $data['allowed_users'] ?? [];
-
-        /** @var list<string> $allowedEnvironments */
-        $allowedEnvironments = $data['allowed_environments'] ?? [];
-
-        /** @var int $percentage */
-        $percentage = $data['percentage'] ?? 100;
-
-        /** @var string $description */
-        $description = $data['description'] ?? '';
-
         return new self(
             name: $name,
             enabled: (bool) ($data['enabled'] ?? false),
-            type: FlagType::from($typeValue),
-            percentage: $percentage,
-            allowedTenants: $allowedTenants,
-            allowedUsers: $allowedUsers,
-            allowedEnvironments: $allowedEnvironments,
-            description: $description,
+            type: FlagType::from($data['type'] ?? 'boolean'),
+            percentage: $data['percentage'] ?? 100,
+            allowedTenants: $data['allowed_tenants'] ?? [],
+            allowedUsers: $data['allowed_users'] ?? [],
+            allowedEnvironments: $data['allowed_environments'] ?? [],
+            description: $data['description'] ?? '',
         );
     }
 
