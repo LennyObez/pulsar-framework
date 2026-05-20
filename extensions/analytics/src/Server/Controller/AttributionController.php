b1f045b6aff360cf86285031ce2a678cac9ef1b6
Lenny Obez <github@lennyobez.com>
2026-05-21 01:09:19 +0200

    fix(analytics): annotate mixed query extracts in AttributionController::calculate and compare

--- extensions/analytics/src/Server/Controller/AttributionController.php
+++ extensions/analytics/src/Server/Controller/AttributionController.php
@@ -26,6 +26,7 @@ final readonly class AttributionController
     public function calculate(ServerRequestInterface $request): Response
     {
         $params = $request->getQueryParams();
+        /** @var mixed $rawSiteId */
         $rawSiteId = $params['site_id'] ?? null;
         $siteId = is_string($rawSiteId) ? $rawSiteId : '';
 
@@ -33,11 +34,14 @@ final readonly class AttributionController
             return Response::json(['error' => 'site_id is required'], 400);
         }
 
+        /** @var mixed $rawFrom */
         $rawFrom = $params['from'] ?? null;
+        /** @var mixed $rawTo */
         $rawTo = $params['to'] ?? null;
         $from = new DateTimeImmutable(is_string($rawFrom) ? $rawFrom : '-30 days');
         $to = new DateTimeImmutable(is_string($rawTo) ? $rawTo : 'now');
 
+        /** @var mixed $rawModel */
         $rawModel = $params['model'] ?? null;
         $modelStr = is_string($rawModel) ? $rawModel : 'last_touch';
         $model = AttributionModel::tryFrom($modelStr);
@@ -46,6 +50,7 @@ final readonly class AttributionController
             return Response::json(['error' => 'Invalid model: ' . $modelStr], 400);
         }
 
+        /** @var mixed $rawGoalId */
         $rawGoalId = $params['goal_id'] ?? null;
         $goalId = is_string($rawGoalId) && $rawGoalId !== '' ? $rawGoalId : null;
 
@@ -65,6 +70,7 @@ final readonly class AttributionController
     public function compare(ServerRequestInterface $request): Response
     {
         $params = $request->getQueryParams();
+        /** @var mixed $rawSiteId */
         $rawSiteId = $params['site_id'] ?? null;
         $siteId = is_string($rawSiteId) ? $rawSiteId : '';
 
@@ -72,7 +78,9 @@ final readonly class AttributionController
             return Response::json(['error' => 'site_id is required'], 400);
         }
 
+        /** @var mixed $rawFrom */
         $rawFrom = $params['from'] ?? null;
+        /** @var mixed $rawTo */
         $rawTo = $params['to'] ?? null;
         $from = new DateTimeImmutable(is_string($rawFrom) ? $rawFrom : '-30 days');
         $to = new DateTimeImmutable(is_string($rawTo) ? $rawTo : 'now');
