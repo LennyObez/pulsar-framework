f2f051b5ca2a05bc72800a393a2272d2df017279
Lenny Obez <github@lennyobez.com>
2026-05-21 05:55:28 +0200

    fix(cache): type result accumulator and annotate mixed value in SimpleCache getMultiple/setMultiple

--- src/Cache/Application/SimpleCache.php
+++ src/Cache/Application/SimpleCache.php
@@ -56,6 +56,7 @@ final readonly class SimpleCache implements CacheInterface
     public function getMultiple(iterable $keys, mixed $default = null): iterable
     {
         $keyList = $this->iterableToArray($keys);
+        /** @var array<string, mixed> $result */
         $result = [];
 
         foreach ($this->pool->getItems($keyList) as $key => $item) {
@@ -70,7 +71,10 @@ final readonly class SimpleCache implements CacheInterface
     {
         $success = true;
 
-        /** @var string $key */
+        /**
+         * @var string $key
+         * @var mixed $value
+         */
         foreach ($values as $key => $value) {
             $item = new CacheItem($key);
             $item->set($value);
